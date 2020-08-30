5f5c8b8b4b14e3cc96cc4f5ad6c6d32aeaa58ae6
Aaron Brady <aleebrady@gmail.com>
2020-08-30 01:06:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,19 +1,14 @@
 require 'pry'
 
 class Anagram
-  
-  attr_accessor :word 
+  attr_accessor :word
   
   def initialize(word)
     @word = word
   end
   
-  def match(words) 
-    words.select do |w|
-      binding.pry
-      w.split("").sort == @word.split("").sort
-    end
+  def match(list)
+    list.select{|word| word.split("").sort == @word.split("").sort}
   end
-
-
+  
 end
