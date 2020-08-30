f9a448b0293f657f2222b931d654692a29ba2c9c
Aaron Brady <aleebrady@gmail.com>
2020-08-30 00:50:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,5 @@
 require 'pry'
 
 class Anagram
-  attr_accessor :word
-  
-  def initialize(word)
-    @word = word
-  end
-  
-  def match(list)
-    list.select{|word| word.split("").sort == @word.split("").sort}
-  end
-  
+
 end
